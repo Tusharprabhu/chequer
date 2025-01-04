2773510682f7bbf6d3dfbd84f6a2402d15ec5a01
Tusharprabhu <tusharprabhu2004@gmail.com>
2025-01-04 20:10:51 +0530

    feat: update MongoDB connection to use environment variables for configuration

--- db.js
+++ db.js
@@ -1,14 +1,18 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://admin:admin@cluster0.sbw1w.mongodb.net/database?retryWrites=true&w=majority");
+    await mongoose.connect(process.env.CONNECTION_STRING, {
+      dbName: process.env.DATABASE_NAME,
+    });
     console.log('MongoDB is connected');
   } catch (error) {
     console.error(error.message);
     process.exit(1);
   }
-}
+};
 
-export { connectDB }; 
-
+export { connectDB };
